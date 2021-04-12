d43a4773cfeaeea45f0dbb85d78735acca0b231f
hslx3y3h <gongjunwei@163.com>
2021-04-12 15:27:07 +0800

    data

--- data/common.inc.php
+++ data/common.inc.php
@@ -2,9 +2,9 @@
 //数据库连接信息
 $cfg_dbtype = 'mysql';
 $cfg_dbhost = 'localhost';
-$cfg_dbname = 'xuexijingyan';
-$cfg_dbuser = 'xuexijingyan';
-$cfg_dbpwd = 'gong@123';
+$cfg_dbname = 'xuexi';
+$cfg_dbuser = 'xuexi';
+$cfg_dbpwd = 'g**@**3';
 $cfg_dbprefix = 'dede_';
 $cfg_db_language = 'utf8';
 
